4a37e2d5269417d801aa99e35a602a62b774be21
ryan30sep <riyan30sep@gmail.com>
2023-03-19 22:32:38 +0700

    Create Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Mohammad Ryan Ahiar
+            Saya tinggal di cimone tangerang
+            olahraga yang saya sukai adalah
+            bermain basket";
+    }
+}
