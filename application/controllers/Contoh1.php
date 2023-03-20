d01080e45af79a9abff8e1d1c4e92802780fe5f9
ryan30sep <riyan30sep@gmail.com>
2023-03-20 16:12:19 +0700

    Pertemuan 2

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,7 +4,7 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Mohammad Ryan Ahiar
+        echo "Nama saya Ryan Ahiar
             Saya tinggal di cimone tangerang
             olahraga yang saya sukai adalah
             bermain basket";
